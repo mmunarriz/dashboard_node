688f28a8305f70b6215ee59a05959650aaf2d745
mmunarriz <mmunarriz@gmail.com>
2022-10-23 19:55:57 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// utilizamos el router
+const router = require('./routes/router')
+app.use(router.routes)
+
+// app.get('/', (req, res)=>{
+//     res.send('Dashboard con Node Js')
+// })
+
+app.listen(3000, ()=> {
+    console.log('Server ip running in http://localhost:3000')
+})
